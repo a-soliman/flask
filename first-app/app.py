8066076ecc0e8762bf8f509e8af7ea1ccf404cd8
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-02 08:30:14 -0700

    working with jsonify

--- first-app/app.py
+++ first-app/app.py
@@ -1,32 +1,56 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+stores = [
+    {
+    'name': 'My wonderfull store',
+    'items': [
+            {
+            'name': 'mug',
+            'price': 7.55
+            },
+            {
+            'name': 'pen',
+            'price': 0.79
+            }
+        ]
+    },
+    {
+    'name': 'Another Store',
+    'items': [
+        {
+        'name': 'laptop',
+        'price': 499.99
+        }
+    ]
+    }
+]
 # POST /store data: {name:}
 @app.route('/store', methods=['POST'])
 def create_store():
-      pass
+    pass
 
 
 # GET /store/<string: name>
 @app.route('/store/<string:name>', methods=['GET'])
 def get_item_in_store(name):
-      pass
+    pass
 
 # GET/store
 @app.route('/store')
 def get_stores():
-      pass
+    return jsonify({'stores': stores})
 
 # POST
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-      pass
+    pass
 
 # GET /store/<string: name>
 @app.route('/store/<string:name>/item', methods=['GET'])
 def get_items_in_store(name):
-      pass
+    pass
 
 
 app.run(port = 5000)
