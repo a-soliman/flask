548fef7b0f84c1a730914e7bd77901bbdd9586a7
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-02 08:19:01 -0700

    coded multiple get and post requirest simulating store app

--- first-app/app.py
+++ first-app/app.py
@@ -2,12 +2,31 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/') # http://www.google.com/
-def home():
-      return 'Hello World!'
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+      pass
 
-@app.route('/about')
-def about():
-      return 'About page...'
-      
+
+# GET /store/<string: name>
+@app.route('/store/<string:name>', methods=['GET'])
+def get_item_in_store(name):
+      pass
+
+# GET/store
+@app.route('/store')
+def get_stores():
+      pass
+
+# POST
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+      pass
+
+# GET /store/<string: name>
+@app.route('/store/<string:name>/item', methods=['GET'])
+def get_items_in_store(name):
+      pass
+
+
 app.run(port = 5000)
