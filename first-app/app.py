dc3a616472ce2eee134a7c84f251c3dc9ea68358
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-02 08:51:54 -0700

    created a POST req + get request to add items and review them through POSTMAN

--- first-app/app.py
+++ first-app/app.py
@@ -44,7 +44,7 @@ def create_store():
 @app.route('/store/<string:name>', methods=['GET'])
 def get_item_in_store(name):
     for store in stores:
-        if store[name] == name:
+        if store['name'] == name:
             return jsonify(store)
     
     return 'Store {}, was not found in our Database'.format(name)
@@ -57,12 +57,24 @@ def get_stores():
 # POST
 @app.route('/store/<string:name>/item', methods=['POST'])
 def create_item_in_store(name):
-    pass
+    request_data = request.get_json()
+
+    for store in stores:
+        if store['name'] == name:
+            store['items'].append(request_data)
+            return jsonify(store)
+    
+    return jsonify({ 'message': 'store was not found'})
+
 
 # GET /store/<string: name>
 @app.route('/store/<string:name>/item', methods=['GET'])
 def get_items_in_store(name):
-    pass
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({'items': store.items})
+        
+    return jsonify({ 'message': 'Store was not found'})
 
 
 app.run(port = 5000)
