1fb2e8d776fea6b856f228b2245b7204b2dd5e31
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-03 08:30:48 -0700

    practicing TEST-FIRST-API

--- section04/code/app.py
+++ section04/code/app.py
@@ -4,10 +4,19 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
+items = []
 
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        for item in items:
+            if item['name'] == name:
+                return {'item': item}
+        return {'message': 'item was not found'}
+    
+    def post(self, name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return {'message': 'created item', 'item': item }
 
-api.add_resource(Student, '/student/<string:name>')
+api.add_resource(Item, '/item/<string:name>')
 app.run(port=5000)
