cb1c9de48a158d09485cdfde0fda575cf49e71dc
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-04-05 06:26:16 -0700

    starting wotking with db sqlite3

---
+++ section05/test.py
@@ -0,0 +1,10 @@
+# import the db
+# init the connection
+# init the cursor
+# create a table 
+# execute the cursor to create the table
+# create a user for teat
+# write the insert query
+# execute the cursor to insert the user
+# commit the checnges
+# close the connection
